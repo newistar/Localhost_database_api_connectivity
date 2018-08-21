460e5a44c4535afed62850c76137e5c3c62f51da
subratgupta <newsubrat@gmail.com>
2018-08-21 11:57:46 +0530

    Initial commit

---
+++ app/src/main/java/com/subratgupta/demo/MainActivity.java
@@ -0,0 +1,91 @@
+package com.subratgupta.demo;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String response = new PostClass().execute("Hello").toString();
+
+        Toast.makeText(getApplicationContext(),response,Toast.LENGTH_SHORT).show();
+
+    }
+
+    private String HttpPost(String myUrl) throws IOException, JSONException {
+        String result = "";
+
+        URL url = new URL(myUrl);
+
+        // 1. create HttpURLConnection
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+        // 2. build JSON object
+        JSONObject jsonObject = buidJsonObject();
+
+        // 3. add JSON content to POST request body
+        setPostRequestContent(conn, jsonObject);
+
+        // 4. make POST request to the given URL
+        conn.connect();
+
+        // 5. return response message
+        return conn.getResponseMessage()+"";
+
+    }
+
+    private JSONObject buidJsonObject() throws JSONException {
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.accumulate("name", "Subrat Gupta");
+        jsonObject.accumulate("email",  "technicalsubrat@gmail.com");
+        jsonObject.accumulate("message",  "I am so happy.");
+
+        return jsonObject;
+    }
+
+    private void setPostRequestContent(HttpURLConnection conn,
+                                       JSONObject jsonObject) throws IOException {
+
+        OutputStream os = conn.getOutputStream();
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+        writer.write(jsonObject.toString());
+        Log.i(MainActivity.class.toString(), jsonObject.toString());
+        writer.flush();
+        writer.close();
+        os.close();
+    }
+
+    class PostClass extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String ... urls) {
+
+            try {
+                return HttpPost("http://192.168.1.14/magnum/public/api/add_contact");
+            }catch (Exception e){
+                Log.e("erro345",e.getMessage());
+            }
+            return "Hi";
+        }
+    }
+
+}
+
